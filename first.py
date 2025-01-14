835e0638a1235a10dd37627abcf75b137a2303f9
ranigour <gourrani868@gmail.com>
2025-01-14 22:22:19 +0530

    first
    Signed-off-by: ranigour <gourrani868@gmail.com>

---
+++ first.py
@@ -0,0 +1,6 @@
+query = "SELECT * FROM users WHERE id = 1;"
+print("Hello, World!")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
---
+++ firstsql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Grade VARCHAR(10)
+);
+INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com');
+
+
