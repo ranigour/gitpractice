d4ec8159991eef87c20a1640365a527ba26b1dd5
RANI  GOUR <gourrani868@gmail.com>
2025-03-03 11:59:13 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,6 +1,22 @@
-query = "SELECT * FROM users WHERE id = 1;"
-print("Hello, World!")
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+i=1
+n=int(input("enter the num:"))
+fact=1
+sum_fact=0
+while(i<=n):i
+if(i%2==0):
+    sum_fact=sum_fact-fact*i
+else:
+  sum_fact=sum_fact+fact*i
+i+=1 
+print(sum_fact)
+menu={
+    'pizza':50,
+    'chai':40,
+    'partha':30,
+    'coffe':80,
+    'burger':60,
+}
+print(menu)
+#greet
+print("welcome to rani hotel")
+print("pizza: Rs50/n chai: Rs40/n partha:30: Rs30/n coffe: Rs80/n burger: Rs60/n ")
